b10144b04e98d58571178974c4e948c133bbd5c5
Janusz Guzowski <janusz.guzowski@gmail.com>
2021-10-10 23:06:13 +0200

    feat: add custom app bar

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+    <app-bar />
 
     <v-main>
       <section class="d-flex justify-center my-5">
@@ -85,6 +54,7 @@ import RateChart from "./components/RateChart.vue";
 import CurrencySelect from "./components/CurrencySelect.vue";
 import DateSelector from "./components/DateSelector.vue";
 import ErrorModal from "./components/ErrorModal.vue";
+import AppBar from "./components/AppBar.vue";
 
 export default {
   name: "App",
@@ -172,6 +142,7 @@ export default {
     CurrencySelect,
     DateSelector,
     ErrorModal,
+    AppBar,
   },
 
   data() {
---
+++ src/components/AppBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar app color="primary" flat dark>
+    <div class="logo">CheckRate!</div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn href="https://github.com/arantasar/shiplink" target="_blank" text>
+      <span class="mr-2">GitHub Repo</span>
+      <v-icon>mdi-open-in-new</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppBar",
+};
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  font-size: 2rem;
+}
+</style>
