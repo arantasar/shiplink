07fd979e9b01ada93e9d94fcb5dc7a158cab58c9
Janusz Guzowski <janusz.guzowski@gmail.com>
2021-10-10 20:45:12 +0200

    feat: style header currencies

--- src/components/TheCurrency.vue
+++ src/components/TheCurrency.vue
@@ -1,20 +1,26 @@
 <template>
   <div
-    class="currency pa-4 ma-2 d-flex"
+    class="currency pa-4 ma-2"
     :class="{ positive: difference > 0, negative: difference < 0 }"
   >
-    <div class="content d-flex flex-column align-center">
-      <div>Flag</div>
-      <div>{{ currency.code }}</div>
-      <div>{{ currency.today }}</div>
-      <div>{{ difference }}</div>
+    <img class="flag" :src="flag" />
+    <div class="d-flex">
+      <div class="content d-flex flex-column">
+        <div class="name font-weight-bold">{{ currency.code }}</div>
+        <div class="today">{{ currency.today }}</div>
+        <div class="difference grey--text">{{ difference }}</div>
+      </div>
+      <the-arrow :difference="difference" />
     </div>
-    <the-arrow :difference="difference" />
   </div>
 </template>
 
 <script>
+import gbp from "@/assets/flags/uk.png";
+import eur from "@/assets/flags/eur.webp";
+import usd from "@/assets/flags/usd.png";
 import TheArrow from "./TheArrow.vue";
+
 export default {
   name: "TheCurrency",
   components: { TheArrow },
@@ -25,6 +31,21 @@ export default {
     difference() {
       return (this.currency.today - this.currency.yesterday).toFixed(4);
     },
+    flag() {
+      let ans = "";
+      switch (this.currency.code) {
+        case "EUR":
+          ans = eur;
+          break;
+        case "GBP":
+          ans = gbp;
+          break;
+        case "USD":
+          ans = usd;
+          break;
+      }
+      return ans;
+    },
   },
 };
 </script>
@@ -33,6 +54,16 @@ export default {
 .currency {
   border: 1px solid lightgrey;
   border-radius: 25px;
+}
+
+.flag {
+  width: 100px;
+  height: 60px;
+  object-fit: contain;
+}
+
+.difference {
+  font-size: 80%;
 }
 
 .positive {
